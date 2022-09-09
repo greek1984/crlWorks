4503c11e0fae088e46f92a2337363e5be2b3908a
greek <greek101284@gmail.com>
2022-09-09 10:08:13 +0800

    done script

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+
+namespace Ctrlwork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] stringArr = new string[5]{ "are", "milk", "84", "to 6", "golden autumn" };
+            string[] stringArr2 = new string [stringArr.Length];
+            void SelectFromArray()
+            {    
+                int index = 0;         
+                for (int i = 0; i < stringArr.Length; i++) 
+                {
+                    if (stringArr[i].Length <= 3) 
+                    {
+                        stringArr2[index] = stringArr[i];
+                        index++;
+                        Console.WriteLine($"{stringArr[i]} ");
+                    }
+                }
+                 
+            }                 
+            SelectFromArray();
+        }
+    
+    }
+       
+}
